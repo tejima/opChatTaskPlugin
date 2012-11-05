755bc2dc0f0787c19fc11f2d414d75fe7cf3ba0c
手嶋守 <tejima@gmail.com>
2012-11-06 00:54:10 +0900

    modify mock  json location

--- test/QUnit/mock.js
+++ test/QUnit/mock.js
@@ -1,13 +1,13 @@
 
 $.mockjax({
   url: '/api.php/community/search.json',
-  proxy: 'community_search.json'
+  proxy: 'test/QUnit/community_search.json'
 });
 
 $.mockjax({
   url: '/api.php/activity/post.json',
   data: { message: "2" },
-  proxy: 'activity_post.json'
+  proxy: 'test/QUnit/activity_post.json'
 });
 
 $.mockjax({
@@ -18,28 +18,28 @@ $.mockjax({
 
 $.mockjax({
   url: '/api.php/activity/post.json',
-  proxy: 'activity_post.json'
+  proxy: 'test/QUnit/activity_post.json'
 });
 
 $.mockjax({
   url: '/api.php/activity/search.json',
-  proxy: 'activity_search.json'
+  proxy: 'test/QUnit/activity_search.json'
 });
 
 $.mockjax({
   url: '/api.php/communityconfig/update.json',
-  proxy: 'communityconfig_update.json'
+  proxy: 'test/QUnit/communityconfig_update.json'
 });
 
 $.mockjax({
   url: '/api.php/communityconfig/search.json',
-  proxy: 'communityconfig_search.json'
+  proxy: 'test/QUnit/communityconfig_search.json'
 });
 $.mockjax({
   url: '/api.php/activity/delete.json',
-  proxy: 'activity_delete.json'
+  proxy: 'test/QUnit/activity_delete.json'
 });
 $.mockjax({
   url: '/api.php/community/member.json',
-  proxy: 'community_member.json'
+  proxy: 'test/QUnit/community_member.json'
 });
