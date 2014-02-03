fc8d7f7ad1ad62a90d04803af2ce14792de2bfce
手嶋守 <tejima@gmail.com>
2014-02-03 14:37:59 +0900

    Rebuild plugin for Bootstrap3

--- apps/pc_frontend/modules/chattask/templates/indexSuccess.php
+++ apps/pc_frontend/modules/chattask/templates/indexSuccess.php
@@ -1,178 +1,72 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <link href="opChatTaskPlugin/css/bootstrap.css" rel="stylesheet">
-  <link href="opChatTaskPlugin/css/custom.css" rel="stylesheet">
-
-  <title></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link href="opChatTaskPlugin/css/bootstrap.css" rel="stylesheet">
+    <link href="opChatTaskPlugin/css/custom.css" rel="stylesheet">
+    <title>ChatTask</title>
     <!-- libs -->
-  <script src="opChatTaskPlugin/js/jquery.js"></script>
-  <script src="opChatTaskPlugin/js/jquery.tmpl.js"></script>
-  <script src="opChatTaskPlugin/js/bootstrap.js"></script>
-  <script src="opChatTaskPlugin/js/shortcut.js"></script>
-<!--
-  <style>
-    body {
-      padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-    }
-  </style>
--->
+    <script src="opChatTaskPlugin/js/jquery.js"></script>
+    <script src="opChatTaskPlugin/js/jquery.tmpl.js"></script>
+    <script src="opChatTaskPlugin/js/bootstrap.js"></script>
+    <script src="opChatTaskPlugin/js/shortcut.js"></script>
 <?php
 use_helper('Javascript');
-
 $jsonData = array(
-  'apiKey' => $sf_user->getMemberApiKey(),
+  'apiKey' =>
+$sf_user->getMemberApiKey(),
   'apiBase' => app_url_for('api', 'homepage'),
 );
 echo javascript_tag('
 var openpne = '.json_encode($jsonData).';
 ');
 ?>
+    <script src="opChatTaskPlugin/js/index.js"></script>
 </head>
-<body>
-<div class="navbar navbar-inverse navbar-static-top">
-  <div class="navbar-inner">
-      <a class="brand" href="#">OpenPNE</a>
 
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
-      <div class="nav-collapse collapse">
-        <ul class="nav pull-right">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
-  </div>
-</div>
-<div id="container_left" class="resizeheight" style="position: absolute;margin: 0px;left: 0px;right: auto;top: 40px;bottom: 30px;z-index: 1;width: 239px;display: block;visibility: visible;overflow: hidden; background: #f5f5f5;">
+<body style="">
+<!-- Wrap all page content here -->
+<div id="wrap">
 
-
-  <div class="navbar">
-    <div class="navbar-inner">
-      <form class="navbar-form pull-left">
-        <input type="text" placeholder="チャット名を検索" style="width: 150px; display: none;">
-      </form>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <a id="chatroom-name" class="navbar-brand" href="#"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#">OpenPNE</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="accordion" id="accordion2">
-  </div>
-</div>
-<div id="container_center" class="resizeheight resizewidth-481" style="position: absolute;margin: 0px;left: 240px;right: 0px;top: 40px;bottom: 30px;z-index: 1;display: block;visibility: visible;overflow: hidden;">
-  <div class="navbar">
-    <div class="navbar-inner">
-      <a class="brand" href="#" id="room_name"></a>
+    <!-- Begin page content -->
+    <div class="container">
+        <div class="row resizeheight-full">
+            <div class="col-sm-4 col-md-3 col-lg-3 bg-success col-left resizeheight-full chatroom-list">
+                <div class="panel-group community-list" id="accordion"></div>
+            </div>
+            <div class="col-sm-8 col-md-7 col-lg-7">
+                <div class="resizeheight-middle chatview"></div>
 
-    <ul class="nav pull-right">
-                <li class="divider-vertical"></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-wrench"></i>設定<b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-                  </div>
-
-
-  </div>
-
-  <div id="chat-view" class="resizeheight-150" style="overflow: scroll;">
-  </div>
-
-  <div id="chat-input" class="row" style="position: absolute;margin: 0px;top: auto;bottom: 0px;left: 0px;right: 0px;width: auto;z-index: 1;height: 80px;display: block;visibility: visible;">
-    <div class="">
-      <textarea id="chat-message" class="span5" name="" cols="30" style="position: absolute;left: 0px;height: 68px; right:0px; width: auto; margin-right: 90px;"></textarea>
+                <div class="chatform">
+                    <form class="form" role="form">
+                        <textarea id="chatform-textarea" class="form-control" rows="4"></textarea>
+                        <button id="chatform-post" type="submit" class="btn btn-success btn-sm btn-block">送信（Shift＋Enterで送信）</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-2 col-lg-2 bg-success resizeheight-full hidden-sm hidden-xs col-right">
+                <p>COL-RIGHT</p>
+            </div>
+        </div>
     </div>
-  
-    <div class="">
-      <button id="chat-post" class="btn  btn-block btn-large" style="position: absolute; top: 5px; right: 5px; width: 80px; height: 70px;"><i class="icon-pencil"></i>送信<br><small>Shift+Enter</small></button>
-    </div>
-  </div>  
 </div>
-<div id="container_right" class="resizeheight" style="position: absolute;margin: 0px;left: auto;right: 0px;top: 40px;bottom: 0px;z-index: 1;width: 240px;display: block;visibility: visible;overflow: hidden; background: #f5f5f5;">
-  <div class="navbar">
-    <div class="navbar-inner">
-      <p class="navbar-text">メモ＆タスク</p>
-    </div>
-  </div>
-
-      <textarea readonly id="info-textarea">
-      </textarea>
-       <div class="btn-group" style="position: relative;">
-        <button id="info-mode-button" class="btn btn-mini">編集モードへ</button>
-        <button id="info-save-button" class="btn btn-mini btn-primary disabled" disabled> SAVE </button>
-      </div>
-      <div id="done" class="alert alert-success" style="display: none;">
-        保存しました！
-      </div>
-
-              Task List
-      <form id="taskdone-form">
-      <div id="task-view">
-      </div>
-      </form>
-
-
-</div>
-
-
-<div style="position: absolute; bottom: 0px; height: 24px; background: #333333; width: auto; left: 0px; right: 0px;">
-  BBBBBBBBBB
-</div>
-
-
-<script src="opChatTaskPlugin/js/index.js"></script>
-<div id="qunit"></div>
-
-
-    <SCRIPT type="text/javascript">
-<!--
-
-function getWindowClientSize(){
-  var result={"width":0,"height":0};
-  if(window.self&&self.innerWidth){
-    result.width=self.innerWidth;
-    result.height=self.innerHeight;
-  }else if(document.documentElement && document.documentElement.clientHeight){
-    result.width=document.documentElement.clientWidth;
-    result.height=document.documentElement.clientHeight;
-  }else{
-    result.width=document.body.clientWidth;
-    result.height=document.body.clientHeight;
-  }
-  return result;
-}
-function doResize(){
-  windowSize = getWindowClientSize();
-  $(".resizeheight").css("height",(windowSize.height - 65) + "px");
-  $(".resizeheight-150").css("height",(windowSize.height - 215) + "px");
-
-  $(".resizewidth-481").css("width",(windowSize.width - 481) + "px");
-
-}
-//-->
-</SCRIPT>
-    <script>
-    $(window).resize(function(){
-      doResize();
-    });
-
-    $(document).ready(function(){
-      doResize();
-    });
-
-
-
-    </script>
 
 </body>
 </html>
--- web/js/index.js
+++ web/js/index.js
@@ -1,175 +1,27 @@
-
-
-
-var active_community = null;
-var memberlist_loaded = false;
-var chatview_loaded = false;
-var community_list = {};
-
-
-var timeline_line = '<div class="row"><div class="span1"><div style="float: left;"><img width="36" height="36" src="${member.profile_image}"></div></div><div style="word-wrap: break-word;"><a href="/member/${member.id}" class="screenmae">${member.name}</a>${body}</div><ul class="nav nav-pills pull-right"><li class="disabled"><a href="#">${created_at}</a></li></ul></div>';
-$.template("timelineTMPL", timeline_line);
-
-var task_line = '<div class="block-task"><div class="row"><div class="span1"><img width="24" height="24" src="${member.profile_image}"></div></div>';
-task_line += '<div class="row"><div class="span3"><small><input type="checkbox" target-id="${id}">${body}</small></div></div><div class="row"><div class="span2 offset1 text-right"><small><a href="">追加時点に移動</a></small></div></div></div>';
-$.template("taskTMPL", task_line);
-
-var chatlist_line = '<div class="accordion-group"><div class="accordion-heading"><img width="36" height="36" src="${community_image_url}" style="float: left;margin-right: 2px;"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse-${id}" target-id="${id}">${name}</a></div>';
-chatlist_line += '<div id="collapse-${id}" class="accordion-body collapse"><div class="accordion-inner" target-id="${id}"></div></div></div>';
-$.template("chatlistTMPL", chatlist_line);
-
-var chatmember_line = '<img class="img-rounded3 pad1" src="${profile_image}" width="24" height="24">';
-$.template("chatmemberTMPL", chatmember_line);
-
-var community = loadCommunityList(false);
-if(community.id >= 1){
-  chatview_loaded = true;
-  active_community = community;
-}else{
-  alert("ネットが不安定です。ブラウザをリロードしてください。" + community.id);
+// JavaScript
+if (!window.console){
+   window.console = { log:function(msg){ }};
 }
 
 
-updateChatRoom();
-$('#chat-view').scrollTop($('#chat-view')[0].scrollHeight - $('#chat-view').height());
-
-$(function(){
-  $("#chat-message").focusin(function(){
-    shortcut.add("Shift+Enter",function() {
-      $("#chat-post").click();
-    });
-  });
-  $("#chat-message").focusout(function(){
-    shortcut.remove("Shift+Enter");
-  });
-
-  $("#chat-post").click(function(){
-    var msg = $("#chat-message").val();
-    if(!msg){
-      return;
-    }
-    $.get('/api.php/activity/post.json',{apiKey: openpne.apiKey,target: "community",target_id: active_community.id, body: msg},function(json){
-      $.tmpl("timelineTMPL",json.data).appendTo("#chat-view");
-      $('#chat-view').scrollTop($('#chat-view')[0].scrollHeight - $('#chat-view').height());
-      $("#chat-message").val("");
-    },"json");
-  });
-
-  $("#info-mode-button").toggle(
-    function(){
-      memoEditMode(false);
-    },
-    function(){
-      memoEditMode(true);
-    }
-  );
-
-  $("#info-save-button").click(function(){
-    var msg = $("#info-textarea").val();
-    $.get('/api.php/communityconfig/update.json',{apiKey: openpne.apiKey , key: 'public_memo', value: msg , community_id: active_community.id} ,function(json){
-      $("#info-textarea").val(msg);
-      $("#done").show();
-      $("#done").animate({opacity: 0.99}, 2000 );
-      $("#done").fadeOut(1000);
-      $("#info-mode-button").text("編集モードへ");
-      $("#info-textarea").attr("readonly",true);
-      $("#info-save-button").attr("disabled","disabled").addClass("disabled");
-    },"json");
-  });
-  $("#taskdone-form :checkbox").live("click",function(){
-    var block = $(this).parents(".block-task");
-    if($(this).attr('checked')) {
-      $.get('/api.php/activity/delete.json',{apiKey: openpne.apiKey ,id: $(this).attr('target-id')},function(json){
-        if(json.result == "success"){
-          block.fadeOut();
-          msg = "タスク完了";
-          $.get('/api.php/activity/post.json',{apiKey: openpne.apiKey,target: "community",target_id: active_community.id, body: msg},function(json){
-            $.tmpl("timelineTMPL",json.data).appendTo("#chat-view");
-            $('#chat-view').scrollTop($('#chat-view')[0].scrollHeight - $('#chat-view').height());
-          },"json");
-        } else {
-          alert("削除できなかった");
-        }
-      });
-    } else {
-    }
-  });
-
-  $(".accordion-toggle").live("click",function(){
-    var targetId = $(this).attr('target-id');
-    active_community = community_list[targetId];
-    memoEditMode(false);
-    updateChatRoom();
-
-    var sleep = 0;
-    if($(".accordion-inner[target-id='"+ targetId +"'] > *").size() > 0){
-      sleep = 30000;
-    }
-    setTimeout(function() {
-      $.get('/api.php/community/member.json',{apiKey: openpne.apiKey ,community_id: targetId},function(json){
-        
-        $(".accordion-inner[target-id='"+ targetId +"'] > *").remove();
-        $.tmpl("chatmemberTMPL",json.data).appendTo(".accordion-inner[target-id='"+ targetId +"']");
-      },"json");
-    }, sleep);
-  });
-
-  setInterval(function() {
-    updateChatRoom();
-  }, 10000);
-
-});
-function memoEditMode(isEdit){
-  if(isEdit){
-    $("#info-mode-button").text("閲覧モードへ");
-    $("#info-textarea").removeAttr("readonly").focus();
-    $("#info-save-button").removeAttr("disabled").removeClass("disabled");
+function getWindowClientSize(){
+  var result={"width":0,"height":0};
+  if(window.self&&self.innerWidth){
+    result.width=self.innerWidth;
+    result.height=self.innerHeight;
+  }else if(document.documentElement && document.documentElement.clientHeight){
+    result.width=document.documentElement.clientWidth;
+    result.height=document.documentElement.clientHeight;
   }else{
-    $("#info-mode-button").text("編集モードへ");
-    $("#info-textarea").attr("readonly","readonly");
-    $("#info-save-button").attr("disabled","disabled").addClass("disabled");
+    result.width=document.body.clientWidth;
+    result.height=document.body.clientHeight;
   }
-}
-
-function loadCommunityList(isAsync){
-  if (isAsync === undefined){
-    isAsync = true;
-  }
-  var result = -1;
-  $.ajax({
-    url: '/api.php/community/search.json',
-    dataType: 'json',
-    data: {apiKey: openpne.apiKey},
-    async: isAsync,
-    success: function(json) {
-      console.log(json.data);
-      json.data = sortChatRoomOrder(json.data);
-      $.tmpl("chatlistTMPL",json.data).appendTo("#accordion2");
-      result = json.data[0];
-      for(var i=0;i < json.data.length;i++){
-        community_list[json.data[i].id] = json.data[i];
-      }
-    }
-  });
   return result;
 }
-function updateChatRoom(){
-  //update memo
-  if($("#info-textarea").attr("readonly")){
-    $.get('/api.php/communityconfig/search.json', {apiKey: openpne.apiKey,community_id: active_community.id,key: 'public_memo'}, function(json) {
-      $("#info-textarea").val(json.data['value']);
-    },"json");
-  }
-  //update room name
-  $("#room_name").text(active_community["name"]);
-
-  //update timeline
-  $.get('/api.php/activity/search.json', {apiKey: openpne.apiKey,target: "community", target_id: active_community.id}, function(json) {
-
-  $("#chat-view").html("");
-  $.tmpl("timelineTMPL",json.data.reverse()).appendTo("#chat-view");
-  //$('#chat-view').scrollTop($('#chat-view')[0].scrollHeight - $('#chat-view').height());
-  },"json");
+function doResize(){
+  windowSize = getWindowClientSize();
+  $(".resizeheight-full").css("height",(windowSize.height - 53) + "px");
+  $(".resizeheight-middle").css("height",(windowSize.height - 53 - 124) + "px");
 }
 
 function sortChatRoomOrder(data){
@@ -191,35 +43,180 @@ function sortChatRoomOrder(data){
       console.log(data);
 
       for(var i=0;i<data.length;i++){
-        //FIXME
-        /*
-        for(var j = 2; j>=0 ; j--){
-          if(data[i].id == order_list[j]){
-            newlist.unshift(data[i]);
-          }
-        }
-        */
-        if(data[i].name == "全員集合"){
-          newlist.unshift(data[i]);
-        }else{
-          newlist.push(data[i]);
-        }
-        //newlist.push("aaa");
+        newlist.push(data[i]);
       }
       console.log("newlist");
       console.log(newlist);
-      //return newlist;
-      //return data;
     }
   });
   return newlist;
 }
+function loadCommunityList(isAsync){
+  if (isAsync === undefined){
+    isAsync = true;
+  }
+  var result = -1;
+  $.ajax({
+    url: '/api.php/community/search.json',
+    dataType: 'json',
+    data: {apiKey: openpne.apiKey},
+    async: isAsync,
+    success: function(json) {
+      console.log(json.data);
+      json.data = sortChatRoomOrder(json.data);
+      $.tmpl("communitylistTMPL",json.data).appendTo(".community-list");
+      result = json.data[0];
+      for(var i=0;i < json.data.length;i++){
+        community_list[json.data[i].id] = json.data[i];
+      }
+    }
+  });
+  return result;
+}
+function updateChatRoom(){
+  //update memo
+  if($("#info-textarea").attr("readonly")){
+    $.get('/api.php/communityconfig/search.json', {apiKey: openpne.apiKey,community_id: active_community.id,key: 'public_memo'}, function(json) {
+      $("#info-textarea").val(json.data['value']);
+    },"json");
+  }
+  //update room name
+  $("#chatroom-name").text(active_community["name"]);
+
+  //update timeline
+  $.get('/api.php/topic_comment/search.json', {apiKey: openpne.apiKey,community_id: active_community.id,count: 30}, function(json) {
+      $(".chatview").html("");
+
+      var newArray = json.data.map(function (value, index, context) {
+          var tmp_body = value.body;
+          value.body = tmp_body.replace(/((http|https):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '<a href="$1">$1</a> ');
+          return value;
+      });
+      $.tmpl("chatTMPL",newArray).appendTo(".chatview");
+      scroll2Bottom(".chatview");
+
+  },"json");
+}
+function memoEditMode(isEdit){
+  if(isEdit){
+    $("#info-mode-button").text("閲覧モードへ");
+    $("#info-textarea").removeAttr("readonly").focus();
+    $("#info-save-button").removeAttr("disabled").removeClass("disabled");
+  }else{
+    $("#info-mode-button").text("編集モードへ");
+    $("#info-textarea").attr("readonly","readonly");
+    $("#info-save-button").attr("disabled","disabled").addClass("disabled");
+  }
+}
+function scroll2Bottom(selector){
+  $(selector).scrollTop($(selector)[0].scrollHeight);
+}
+
+var active_community = null;
+var memberlist_loaded = false;
+var chatview_loaded = false;
+var community_list = {};
+
+$(window).resize(function(){
+  doResize();
+});
+$(document).ready(function(){
+
+  var chat_line = '<div class="row row-chatsingle"><div class="col-lg-1"><div class="div-chatmember"><img src="${member.profile_image}" width="36" height="36" class="img-rounded" alt=""></div></div><div class="col-lg-11"><div class="row row-chatmemberinfo"><a href="/member/${member.id}" class="screenname"><div class="col-lg-8">${member.name}</div></a><div class="col-lg-4"><div class="pull-right">${created_at}</div></div></div><div class="row"><div class="col-lg-11">{{html body}}</div></div></div></div>';
+  $.template("chatTMPL", chat_line);
+
+  var task_line = '<div class="block-task"><div class="row"><div class="col-lg-1"><img width="24" height="24" src="${member.profile_image}"></div></div>';
+  task_line += '<div class="row"><div class="col-lg-3"><small><input type="checkbox" target-id="${id}">${body}</small></div></div><div class="row"><div class="col-lg-2 offset1 text-right"><small><a href="">追加時点に移動</a></small></div></div></div>';
+  $.template("taskTMPL", task_line);
+
+  var communitytmpl_list = '<div class="panel panel-default"><div class="panel-heading panel-chatroom-list"><h5 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse-${id}" target-id="${id}"><img width="36" height="36" src="${community_image_url}" alt="">${name}</a></h5></div><div id="collapse-${id}" class="panel-collapse collapse"><div class="panel-body"></div></div></div>';
+  $.template("communitylistTMPL", communitytmpl_list);
+
+  var chatmember_line = '<img class="img-rounded3 pad1 tooltip-member" src="${profile_image}" width="24" height="24" data-toggle="tooltip" data-placement="top" title="${name}">';
+  $.template("chatmemberTMPL", chatmember_line);
+
+  var community = loadCommunityList(false);
+  if(community.id >= 1){
+    chatview_loaded = true;
+    active_community = community;
+  }else{
+    alert("ネットが不安定です。ブラウザをリロードしてください。" + community.id);
+  }
+  updateChatRoom();
+
+  $(function(){
+    $("#chatform-textarea").focusin(function(){
+      shortcut.add("Shift+Enter",function() {
+        $("#chatform-post").click();
+      });
+    });
+    $("#chatform-textarea").focusout(function(){
+      shortcut.remove("Shift+Enter");
+    });
+
+    $("#chatform-post").click(function(event){
+      event.preventDefault();
+      var msg = $("#chatform-textarea").val();
+      if(!msg){
+        return;
+      }
+      $.get('/api.php/topic_comment/post.json',{apiKey: openpne.apiKey,community_id: active_community.id, body: msg},function(json){
+
+        json.data.body = json.data.body.replace(/((http|https):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '<a href="$1">$1</a> ');
+        $.tmpl("chatTMPL",json.data).appendTo(".chatview");
+        scroll2Bottom(".chatview");
+        $("#chatform-textarea").val("");
+      },"json");
+    });
 
 
+    $("#info-mode-button").toggle(
+      function(){
+        memoEditMode(false);
+      },
+      function(){
+        memoEditMode(true);
+      }
+      );
 
+    $("#info-save-button").click(function(){
+      var msg = $("#info-textarea").val();
+      $.get('/api.php/communityconfig/update.json',{apiKey: openpne.apiKey , key: 'public_memo', value: msg , community_id: active_community.id} ,function(json){
+        $("#info-textarea").val(msg);
+        $("#done").show();
+        $("#done").animate({opacity: 0.99}, 2000 );
+        $("#done").fadeOut(1000);
+        $("#info-mode-button").text("編集モードへ");
+        $("#info-textarea").attr("readonly",true);
+        $("#info-save-button").attr("disabled","disabled").addClass("disabled");
+      },"json");
+    });
 
+    $(document).on("click", ".panel-title a", function(){
+      var targetId = $(this).attr('target-id');
+      active_community = community_list[targetId];
+      memoEditMode(false);
+      updateChatRoom();
 
+      var sleep = 0;
+      if($(".accordion-inner[target-id='"+ targetId +"'] > *").size() > 0){
+        sleep = 30000;
+      }
+      setTimeout(function() {
+        $.get('/api.php/community/member.json',{apiKey: openpne.apiKey ,community_id: targetId},function(json){
 
+          $("#collapse-"+ targetId +" .panel-body > *").remove();
 
+          $.tmpl("chatmemberTMPL",json.data).appendTo("#collapse-" +targetId+ " .panel-body");
+          $(".tooltip-member").tooltip();
 
+        },"json");
+      }, sleep);
+    });
+    setInterval(function() {
+      updateChatRoom();
+    }, 20000);
 
+  });
+doResize();
+});
